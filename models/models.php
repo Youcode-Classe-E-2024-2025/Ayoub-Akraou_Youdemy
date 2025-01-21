48de6892c480fa53c8faf933cbbe118a30c07598
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-01-21 23:45:11 +0100

    Add models index file for autoloading

---
+++ models/models.php
@@ -0,0 +1,8 @@
+<?php require_once('models/User.php'); ?>
+<?php require_once('models/Admin.php'); ?>
+<?php require_once('models/Student.php'); ?>
+<?php require_once('models/Teacher.php'); ?>
+<?php require_once('models/Course.php'); ?>
+<?php require_once('models/Category.php'); ?>
+<?php require_once('models/Tag.php'); ?>
+<?php require_once('models/Enrollment.php'); ?>
