2dbf8ce8cc0e37e7732525acbef1556368746b0a
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-01-21 23:39:21 +0100

    Add helper functions for common utilities

---
+++ helpers/functions.php
@@ -0,0 +1,14 @@
+<?php
+function dd($value)
+{
+   echo '<pre class="font-medium min-w-56 bg-black text-white z-40 w-fit mx-auto shadow-md shadow-white p-4 max-w-full text-wrap">';
+   var_dump($value);
+   echo '</pre>';
+   // die();
+}
+
+function checkURL($value)
+{
+   $path = parse_url($_SERVER['REQUEST_URI'])['path'];
+   return $value === $path;
+}
